df7d29ea07b72edc07a42cb7b2914a73db991f13
unknown <may.alex43@gmail.com>
2018-09-04 20:06:31 -0400

    stock prices

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -2,8 +2,24 @@
 
 import argparse
 
-def find_max_profit(prices):
-  pass 
+def find_max_profit(prices,n):
+  profit=[0]
+  max_price=prices[n-1]
+
+  for i in range (n-2, 0, -1):
+    if prices[i]>max_price:
+      max_price=prices[i]
+      profit[i]= max(profit[i+1], max_price-prices[i])
+  
+  min_price=prices[0]
+  for i in range(1,n):
+    if price[i]< min_price:
+      min_price=prices[i]
+    
+    profit[i]=max(profit[i-1], profit[i]+(prices[i]-min_price))
+
+  result= profit[n-1]
+  return result
 
 
 if __name__ == '__main__':
